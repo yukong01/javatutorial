ee38492d97f155f2f7701ba5085dcb36a6cd9cdf
Mirim <bust102@naver.com>
2015-08-27 19:48:21 +0900

    first

---
+++ src/com/company/t04_daemon/Main.java
@@ -0,0 +1,22 @@
+package com.company.t04_daemon;
+
+/**
+ * Created by Mirim on 2015-08-25.
+ */
+public class Main {
+    public static void main(String[] args) {
+        class DeamonThread extends Thread{
+            public void run(){
+                try {
+                    sleep(Long.MAX_VALUE);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        DeamonThread t = new DeamonThread();
+        t.setDaemon(true);
+        t.start();
+    }
+}
